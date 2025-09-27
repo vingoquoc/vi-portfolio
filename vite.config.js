3899bbdb0af1d406458b8ab765a0837b5a086764
Vi Ngo Quoc <vingoquoc@gmail.com>
2025-09-27 13:36:52 +0700

    Configure for GitHub Project Pages: Update base path to /vi-portfolio/

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages
+  base: '/vi-portfolio/', // Important for GitHub Pages Project
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
