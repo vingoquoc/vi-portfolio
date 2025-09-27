41d93178bd059e992a03981d2aea42a17114d0a8
Vi Ngo Quoc <vingoquoc@gmail.com>
2025-09-27 14:04:47 +0700

    Setup custom domain nqvi.dev: Add CNAME file and update vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vi-portfolio/', // Important for GitHub Pages Project
+  base: './', // Important for Custom Domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
